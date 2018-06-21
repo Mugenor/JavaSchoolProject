b092430ad0ca3801da45728d616e1e81c5b87cc6
Mugenor <ilya.cherno8@mail.ru>
2018-06-21 23:34:08 +0300

    database configuration with entity manager

--- src/main/java/javaschool/config/Config.java
+++ src/main/java/javaschool/config/Config.java
@@ -5,12 +5,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -42,27 +47,57 @@ public class Config {
         return dataSource;
     }
 
+    //    @Bean
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//
+//        sessionFactory.setHibernateProperties(hibernateProperties());
+//        sessionFactory.setDataSource(dataSource());
+//        sessionFactory.setPackagesToScan(env.getRequiredProperty(ENTITY_PACKAGES_TO_SCAN));
+//
+//        return sessionFactory;
+//    }
+//@Bean
+//public HibernateTransactionManager transactionManager() {
+//    HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//    transactionManager.setSessionFactory(sessionFactory().getObject());
+//
+//    return transactionManager;
+//}
     @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(ENTITY_PACKAGES_TO_SCAN));
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setJpaProperties(hibernateProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+
+    private Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.put(HIBERNATE_DIALECT, env.getRequiredProperty(HIBERNATE_DIALECT));
         hibernateProperties.put(HIBERNATE_SHOW_SQL, env.getRequiredProperty(HIBERNATE_SHOW_SQL));
         hibernateProperties.put("hibernate.transaction.flush_before_completion", "true");
         hibernateProperties.put("hibernate.hbm2ddl.auto", "create");
 
-        sessionFactory.setHibernateProperties(hibernateProperties);
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan(env.getRequiredProperty(ENTITY_PACKAGES_TO_SCAN));
-
-        return sessionFactory;
-    }
-
-    @Bean
-    public HibernateTransactionManager transactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-
-        return transactionManager;
+        return hibernateProperties;
     }
 }
--- src/main/java/javaschool/dao/PassengerDAOImpl.java
+++ src/main/java/javaschool/dao/PassengerDAOImpl.java
@@ -1,31 +1,39 @@
 package javaschool.dao;
 
 import javaschool.entity.Passenger;
+import javaschool.service.PassengerService;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.jpa.criteria.CriteriaBuilderImpl;
+import org.hibernate.jpa.criteria.CriteriaQueryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Repository
 public class PassengerDAOImpl implements PassengerDAO {
-    private SessionFactory sessionFactory;
+    private EntityManager entityManager;
 
-    @Autowired
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     public void save(Passenger passenger) {
-        Session session = sessionFactory.getCurrentSession();
-        session.save(passenger);
+        entityManager.persist(passenger);
     }
 
     @SuppressWarnings("unchecked")
     public List<Passenger> list() {
-        Session session = this.sessionFactory.getCurrentSession();
-        List<Passenger> passengers = session.createCriteria(Passenger.class).list();
-        return passengers;
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Passenger> criteriaQuery = criteriaBuilder.createQuery(Passenger.class);
+        criteriaQuery.select(criteriaQuery.from(Passenger.class));
+        return entityManager.createQuery(criteriaQuery).getResultList();
     }
 }
